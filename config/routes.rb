278d609459a8dd6f9dee8c8f9c8c08f5315e550e
lisandroseia <lisandroseia11@gmail.com>
2022-07-12 23:07:35 -0300

    Setup api

---
+++ config/routes.rb
@@ -0,0 +1,3 @@
+Rails.application.routes.draw do
+  get 'greetings', to: 'greetings#index'
+end
---
+++ db/seeds.rb
@@ -0,0 +1,4 @@
+Greeting.create(text: 'first')
+Greeting.create(text: 'second')
+Greeting.create(text: 'third')
+Greeting.create(text: 'fourth')
